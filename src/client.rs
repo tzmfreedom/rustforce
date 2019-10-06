be2bf70aefbd642decf68e37ff8326ad08a8f457
tzmfreedom <makoto_tajitsu@hotmail.co.jp>
2019-10-06 11:23:55 +0900

    impl query

--- src/client.rs
+++ src/client.rs
@@ -1,10 +1,21 @@
 extern crate reqwest;
 
 use reqwest::header::HeaderMap;
-use serde::Deserialize;
+use serde::{Deserialize, Serialize};
+use self::reqwest::header::AUTHORIZATION;
+use serde::export::fmt::Debug;
+use serde::de::DeserializeOwned;
+use self::reqwest::{Response, Error};
 
-#[derive(Deserialize)]
-#[derive(Debug)]
+#[derive(Deserialize, Debug)]
+#[serde(rename_all = "camelCase")]
+pub struct QueryResponse<T> {
+    total_size: i32,
+    done: bool,
+    records: Vec<T>,
+}
+
+#[derive(Deserialize, Debug)]
 pub struct TokenResponse {
     id: String,
     issued_at: String,
@@ -15,22 +26,36 @@ pub struct TokenResponse {
 }
 
 #[derive(Debug)]
+pub struct AccessToken {
+    token_type: String,
+    value: String,
+    issued_at: String,
+}
+
+#[derive(Debug)]
 pub struct Client {
+    http_client: reqwest::Client,
     client_id: String,
     client_secret: String,
     login_endpoint: String,
-    access_token: Option<String>,
+    instance_url: Option<String>,
+    access_token: Option<AccessToken>,
     reflesh_token: Option<String>,
+    version: String,
 }
 
 impl Client {
     pub fn new(client_id: String, client_secret: String) -> Client {
+        let http_client = reqwest::Client::new();
         return Client {
-            client_id: client_id,
-            client_secret: client_secret,
+            http_client,
+            client_id,
+            client_secret,
             login_endpoint: "https://login.salesforce.com".to_string(),
             access_token: None,
             reflesh_token: None,
+            instance_url: None,
+            version: "v44.0".to_string(),
         }
     }
 
@@ -43,13 +68,58 @@ impl Client {
             ("username", username.as_str()),
             ("password", password.as_str()),
         ];
-        let client = reqwest::Client::new();
-        let res: TokenResponse = client.post(token_url.as_str())
+        let res: TokenResponse = self.http_client.post(token_url.as_str())
             .form(&params)
             .send()
             .unwrap()
             .json()
             .unwrap();
-        self.access_token = Some(res.access_token);
+
+        self.access_token = Some(AccessToken {
+            value: res.access_token,
+            issued_at: res.issued_at,
+            token_type: res.token_type,
+        });
+        self.instance_url = Some(res.instance_url);
+    }
+
+    pub fn query<A: Debug + DeserializeOwned>(&self, query: String) -> QueryResponse<A>
+    {
+        let query_url = format!("{}/services/data/{}/query/", self.instance_url.as_ref().unwrap(), self.version);
+        let params = vec![("q", query)];
+        let res = self.get(query_url, params)
+            .unwrap()
+            .json()
+            .unwrap();
+        return res;
+    }
+
+    pub fn create<A: Serialize>(&self, sobject_name: &str, params: A) {
+        let resource_url= format!("{}/services/data/{}/sobjects/{}", self.instance_url.as_ref().unwrap(), self.version, sobject_name);
+        let res = self.post(resource_url, params)
+            .unwrap()
+            .json()
+            .unwrap();
+        return res;
+    }
+
+    fn get(&self, url: String, params: Vec<(&str, String)>) -> Result<Response, Error> {
+        return self.http_client.get(url.as_str())
+            .headers(self.create_header())
+            .query(&params)
+            .send();
+    }
+
+    fn post<T: Serialize>(&self, url: String, body: T) -> Result<Response, Error> {
+        return self.http_client.post(url.as_str())
+            .headers(self.create_header())
+            .json(&body)
+            .send();
+    }
+
+    fn create_header(&self) -> HeaderMap {
+        let mut headers = HeaderMap::new();
+        headers.insert(AUTHORIZATION, format!("Bearer {}", self.access_token.as_ref().unwrap().value).parse().unwrap());
+        return headers;
     }
 }
--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,34 @@
-use rustforce::client::Client;
+use rustforce::client::{Client, QueryResponse};
 use std::env;
 use std::env::VarError;
+use serde::Deserialize;
 
-fn main() -> Result<(), VarError> {
-    let client_id = env::var("SFDC_CLIENT_ID")?;
-    let client_secret = env::var("SFDC_CLIENT_SECRET")?;
-    let username = env::var("SFDC_USERNAME")?;
-    let password = env::var("SFDC_PASSWORD")?;
+#[derive(Deserialize, Debug)]
+#[serde(rename_all = "PascalCase")]
+struct Account {
+    #[serde(rename = "attributes")]
+    attributes: Attribute,
+    id: String,
+    name: String,
+}
+
+#[derive(Deserialize, Debug)]
+struct Attribute {
+    url: String,
+    #[serde(rename = "type")]
+    sobject_type: String,
+}
+
+fn main() {
+    let client_id = env::var("SFDC_CLIENT_ID").unwrap();
+    let client_secret = env::var("SFDC_CLIENT_SECRET").unwrap();
+    let username = env::var("SFDC_USERNAME").unwrap();
+    let password = env::var("SFDC_PASSWORD").unwrap();
 
     let mut client = Client::new(client_id, client_secret);
     client.login_with_credential(username, password);
-    println!("{:?}", client);
-    Ok(())
+//    println!("{:?}", client);
+    client.create("Account", [("name", "create from rust")]);
+    let res: QueryResponse<Account> = client.query("SELECT Id, Name FROM Account ORDER BY CreatedDate DESC LIMIT 1".to_string());
+    println!("{:?}", res);
 }
